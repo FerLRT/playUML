d7424f30103dd5d0b0f9af18fe8cddcbb08c5c61
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-02-10 20:44:59 +0100

    Ver imágenes en pantalla completa

--- client/src/components/DiagramModal.jsx
+++ client/src/components/DiagramModal.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 import Modal from "@mui/material/Modal";
+import {
+  TransformWrapper,
+  TransformComponent,
+  useControls,
+} from "react-zoom-pan-pinch";
 
 import "../styles/diagramModal.css";
 
 export function DiagramModal({ open, handleClose, image }) {
   return (
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <div className="diagram-modal-image-container">
-        <img src={image} alt="Diagrama UML" width="auto" height="auto" />
-      </div>
-    </Modal>
+    <div className="diagram-modal-container">
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className="diagram-modal-image-container">
+          <TransformWrapper
+            maxScale={3}
+            smooth="true"
+            pinch={{ step: 10 }}
+            className="diagram-modal-custom-transform-wrapper"
+          >
+            <Controls />
+            <TransformComponent>
+              <img
+                className="diagram-modal-image"
+                src={image}
+                alt="Diagrama UML"
+                width="auto"
+                height="auto"
+              />
+            </TransformComponent>
+          </TransformWrapper>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+function Controls() {
+  const { zoomIn, zoomOut, resetTransform } = useControls();
+  return (
+    <div className="diagram-modal-controls">
+      <button onClick={() => zoomIn()}>Zoom In</button>
+      <button onClick={() => zoomOut()}>Zoom Out</button>
+      <button onClick={() => resetTransform()}>Reset</button>
+    </div>
   );
 }
