2e2cf0be3563280a6fd035d7d10dd5e1c521665b
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-04-16 17:31:49 +0200

    Control de errores cargar y crear clases

--- client/src/hooks/useClass.js
+++ client/src/hooks/useClass.js
@@ -1,17 +1,13 @@
 import { instance } from "./axiosInstance";
 
 export async function getTeacherClasses(email) {
-  return await instance
-    .get(`/classes/${email}`)
-    .then((response) => response.data)
-    .catch((error) => console.error("Error al obtener clases:", error));
+  return await instance.get(`/classes/${email}`).then((response) => response);
 }
 
 export async function createClass(name, teacherEmail, fileData) {
   return await instance
     .post("/classes", { name, teacherEmail, fileData })
-    .then((response) => response.data)
-    .catch((error) => console.error("Error al crear clase:", error));
+    .then((response) => response);
 }
 
 export async function addStudentToClass(classId, studentEmail) {
