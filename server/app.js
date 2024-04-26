07a63a0ca1b59285addf7982557ba42840cfb8bf
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-04-26 21:03:46 +0200

    Prueba cors con vercel.json

--- server/app.js
+++ server/app.js
@@ -1,13 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import cors from "cors";
 
-import "dotenv/config";
-
-// Routes
+// Importa las rutas
 import { authRouter } from "./routes/auth-router.js";
-
 import { quizRouter } from "./routes/quiz-router.js";
 import { questionRouter } from "./routes/question-router.js";
 import { answerRouter } from "./routes/answer-router.js";
@@ -18,58 +14,17 @@ import { userAchievementRouter } from "./routes/userAchievement-router.js";
 import { userQuizRouter } from "./routes/userQuiz-router.js";
 import { classRouter } from "./routes/class-router.js";
 
+// Crea la instancia de la aplicación Express
 export const app = express();
 
-const allowCors = (fn) => async (req, res) => {
-  const whiteList = [process.env.ORIGIN1];
-
-  const origin = req.headers.origin;
-
-  console.log("Origin:", origin);
-  console.log("WhiteList:", whiteList);
-  console.log("Headers:", req.headers);
-
-  if (whiteList.includes(origin)) {
-    // Si el origen de la solicitud está en la lista blanca, se permite la solicitud
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
-
-    if (req.method === "OPTIONS") {
-      res.status(200).end();
-      return;
-    }
-
-    return await fn(req, res);
-  } else {
-    // Si el origen de la solicitud no está en la lista blanca, se rechaza la solicitud
-    res
-      .status(403)
-      .send("Solicitud no autorizada debido a restricciones de CORS.");
-  }
-};
-
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-
-// Aplica el middleware allowCors a todas las rutas
-app.use(allowCors(handler));
-
+// Middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Aplica las rutas
 app.use("/auth", authRouter);
-
 app.use("/quizzes", quizRouter);
 app.use("/questions", questionRouter);
 app.use("/answers", answerRouter);
