d46fa899eeb00ea0e5c17827e0d5b3352ecd7abb
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-04-26 23:06:12 +0200

    Eliminar www y crear index para vercel

--- server/index.js
+++ server/index.js
@@ -34,3 +34,59 @@ app.use("/achievements", achievementRouter);
 app.use("/user-achievements", userAchievementRouter);
 app.use("/user-quizzes", userQuizRouter);
 app.use("/classes", classRouter);
+
+// Normaliza el puerto en el que escuchará el servidor
+const port = normalizePort(process.env.PORT || "8080");
+app.set("port", port);
+
+// Crea el servidor HTTP
+const server = http.createServer(app);
+
+// Escucha en el puerto especificado
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+// Función para normalizar el puerto
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val; // Pipe
+  }
+
+  if (port >= 0) {
+    return port; // Número de puerto
+  }
+
+  return false;
+}
+
+// Función para manejar errores de servidor
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+// Función para manejar eventos de escucha del servidor
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Listening on " + bind);
+}
