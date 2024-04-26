aa7f543fa085134591c4c0ea48d9f7646f436d14
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-04-26 18:04:41 +0200

    vite config

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
