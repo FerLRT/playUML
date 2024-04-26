78cc6fe5d9065d0bac2d6585f1bb030abd494bf8
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-04-26 20:32:12 +0200

    Cors mejorado

--- server/app.js
+++ server/app.js
@@ -20,21 +20,20 @@ import { classRouter } from "./routes/class-router.js";
 
 export const app = express();
 
-// const whiteList = [process.env.ORIGIN1];
+const whiteList = [process.env.ORIGIN1];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       console.log("😲😲😲 =>", origin);
-//       if (!origin || whiteList.includes(origin)) {
-//         return callback(null, origin);
-//       }
-//       return callback("Error de CORS origin: " + origin + " No autorizado!");
-//     },
-//     credentials: true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      console.log("😲😲😲 =>", origin);
+      if (!origin || whiteList.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback("Error de CORS origin: " + origin + " No autorizado!");
+    },
+    credentials: true,
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
