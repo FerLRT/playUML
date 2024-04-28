449ede1cd641a844d13aba56c23370a39221f69e
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-04-28 12:52:38 +0200

    cors añadido

--- server/index.js
+++ server/index.js
@@ -24,6 +24,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://play-uml.vercel.app",
+    credentials: true,
+  })
+);
 app.options(
   "https://play-uml.vercel.app",
   cors({
