b0393d9568eba468ab7fb1a09155e7c75d667b65
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-04-29 12:09:43 +0200

    Config puertos

--- client/src/hooks/axiosInstance.js
+++ client/src/hooks/axiosInstance.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL ?? "http://localhost:8080",
+  baseURL: import.meta.env.VITE_BASE_URL ?? "http://localhost:3000",
   withCredentials: true,
 });
 
 export const instanceWithAuth = (token) =>
   axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL ?? "http://localhost:8080",
+    baseURL: import.meta.env.VITE_BASE_URL ?? "http://localhost:3000",
     withCredentials: true,
     headers: {
       Authorization: `Bearer ${token}`,
