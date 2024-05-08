324b3e28e565d3f90924b8aeaa64eebc91343a81
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-05-08 14:17:56 +0200

    Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 3001,
     proxy: {
       "/api": {
         target: process.env.VITE_BASE_URL || "http://localhost:3000",
